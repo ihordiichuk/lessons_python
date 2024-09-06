335d4e3e630676e69ed97f64d9b523e9376acb36
it <lordwayden@gmail.com>
2024-09-06 15:30:42 +0300

    fix ignore

---
+++ lessons_python/lesson_1.py
@@ -0,0 +1,12 @@
+# a = 2
+# b = 3
+# c = a + b
+# print(c)
+x = 'Hello'        # x тепер є числом з плаваючою точкою (float)
+print(type(x))
+x=str(x)
+print(type(x))
+y = 3
+y=str(y)
+b = 2*x+y
+print(b)
---
+++ lessons_python/lesson_2.py
@@ -0,0 +1,35 @@
+#data types
+
+#numeric
+
+# int - integer - 1
+# float - 1.3 - 0.(some amount of numbers)01 --> Can start to round the numbers
+# decimal - 0.01 - 0.00000000000001
+# complex - (1 + 3j)
+
+# bool - logic operator (True/False)
+# str
+
+# list() змінна
+# tuple() не змінна
+# dict() змінний
+# set() змінний
+
+# range
+
+# None
+
+# box = "apples" # змінна = "літерал"
+# box_number_1 = True # snake_case
+
+# num_1 = 5
+# z=num_1
+# num_1 = 3
+# num_2 = num_1
+# print(num_2,z)
+
+
+list_1 = [1,2,3]
+list_2 = list_1
+list_1 = [1,2,4]
+print(list_2)
